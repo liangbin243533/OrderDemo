cd59cdda897531f7ed39da5cd86de32c885aedf8
LiangbinChen <chenliangbin123456@126.com>
2021-08-28 09:05:24 +0800

    Test again

--- my-oder/src/components/OrderItem/index.js
+++ my-oder/src/components/OrderItem/index.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import './style.css'
+import React, { Component } from "react";
+import "./style.css"
 
 class OrderItem extends Component {
     render() {
+        const { shop, product, price, picture } = this.props.data; 
         return (
-            <div className='orderItem'>
-                <div className='oderItem__picContainer--red'>
-                    <img className='oderItem__pic'></img>
+            <div className="orderItem">
+                <div className="oderItem__picContainer--red">
+                    <img className="oderItem__pic" ></img>
                 </div>
-                <div className='oderItem__content'>
-                    <div className='oderItem__product'>Product Name</div>
-                    <div className='oderItem__shop'>Shop Name</div>
+                <div className="oderItem__content">
+                    <div className="oderItem__product">{product}</div>
+                    <div className="oderItem__shop">{shop}</div>
                     <div>
-                        <div className='oderItem__price'>13</div>
-                        <div><button className='oderItem__btn 
-                        oderItem__btn--red'>Comment</button></div>
+                        <div className="oderItem__price">13</div>
+                        <div><button className="oderItem__btn 
+                        oderItem__btn--red">Comment</button></div>
                     </div>
 
                 </div>
